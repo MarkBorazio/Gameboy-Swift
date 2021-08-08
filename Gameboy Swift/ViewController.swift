3c4e01fe3dbb950676f3748c27a0858237c17141
Mark <markborazio@hotmail.com>
2021-08-08 15:50:14 +1000

    Initial Commit

---
+++ Gameboy Swift/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Gameboy Swift
+//
+//  Created by Mark Borazio [Personal] on 8/8/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
